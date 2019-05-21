d7fdd2532ca2ef7d869ea57e182e3928b5ca6e35
Matias Mirko <mmffalkiewicz@gmail.com>
2019-05-21 00:06:44 -0300

    implementacion de sites

--- src/App.js
+++ src/App.js
@@ -6,38 +6,36 @@ import Categoria from './categoria.js';
 var React = require('react');
 var ReactDom = require('react-dom');
 
-class Categorias extends React.Component{
+class App extends React.Component{
   constructor(props){
     super(props);
     this.state={
       categories:[],
       sites:[],
       clicked:false,
-      index:null
+      index:null,
+      flag_categorias:false
     };
     this.verCategoria=this.verCategoria.bind(this);
     this.verCategorias=this.verCategorias.bind(this);
   }
   componentDidMount() {
-    // fetch('https://cors-anywhere.herokuapp.com/api.mercadolibre.com/sites')
-    //     .then(response => response.json())
-    //     .then(sites => {
-    //       console.log(sites);
-    //       this.setState({
-    //         sites:sites
-    //       })
-    //     });
-    fetch('https://cors-anywhere.herokuapp.com/api.mercadolibre.com/sites/MLA/categories')
+    fetch('https://cors-anywhere.herokuapp.com/api.mercadolibre.com/sites')
         .then(response => response.json())
-        .then(categorias => {
-          console.log(categorias);
+        .then(sites => {
+          console.log(sites);
           this.setState({
-            categories:categorias
+            sites:sites
           })
         });
+
   }
   verCategoria(index){
     console.log("entró");
+    var select=document.getElementById("s_sites");
+    ReactDom.findDOMNode(select).style.display="none";
+    var titulo=document.getElementById("titulo");
+    ReactDom.findDOMNode(titulo).style.display="none";
     this.setState({
       index:index,
       clicked:true
@@ -46,9 +44,24 @@ class Categorias extends React.Component{
   }
   verCategorias(){
     console.log("entró");
+    var titulo=document.getElementById("titulo");
+    ReactDom.findDOMNode(titulo).style.display="block";
+    var select=document.getElementById("s_sites");
+    ReactDom.findDOMNode(select).style.display="block";
+    var siteId=document.getElementById("s_sites").value;
+    console.log(siteId);
+    fetch('https://cors-anywhere.herokuapp.com/api.mercadolibre.com/sites/'+ siteId +'/categories')
+        .then(response => response.json())
+        .then(categorias => {
+          console.log(categorias);
+          this.setState({
+            categories:categorias
+          })
+        });
     this.setState({
       index:null,
-      clicked:false
+      clicked:false,
+      flag_categorias:true
     });
     //ReactDom.render(<Categoria id={id} name={name}/>, document.getElementById('root'))
   }
@@ -56,35 +69,37 @@ class Categorias extends React.Component{
 
   render() {
     var vista;
-    if (!this.state.clicked) {
+    if(this.state.flag_categorias) {
+      if (!this.state.clicked) {
         vista = <div>
-                   <h2>Lista de categorías</h2>
-                   <ul className="list-group">
-                      {this.state.categories.map((cat, index) =>
-                          <li className="list-group-item" key={index} onClick={this.verCategoria.bind(this, index)}>
-                            {cat.name}
-                          </li>
-                      )}
-                   </ul>
-                </div>
-    } else {
-       vista = <div>
-                  <Categoria id={this.state.categories[this.state.index].id}/>
-                  <button className="btn btn-primary" onClick={this.verCategorias}>VOLVER</button>
-               </div>
+          <ul className="list-group">
+            {this.state.categories.map((cat, index) =>
+                <li className="list-group-item" key={index} onClick={this.verCategoria.bind(this, index)}>
+                  {cat.name}
+                </li>
+            )}
+          </ul>
+        </div>
+      } else {
+        vista = <div>
+          <Categoria id={this.state.categories[this.state.index].id}/>
+          <button className="btn btn-primary" onClick={this.verCategorias}>VOLVER</button>
+        </div>
+      }
     }
-
     return (
         <div className="container">
+          <h2 id="titulo">Lista de categorías</h2>
+          <select id="s_sites" onChange={this.verCategorias}>
+            {this.state.sites.map((sites,index) =>
+                <option key={index} value={sites.id}>{sites.name}</option>
+            )}
+          </select>
           {vista}
         </div>
     );
   }
 
 }
-//
-// Categorias.defaultProps={
-//   nombre: "Matias"
-// };
 
-export default Categorias;
+export default App;
